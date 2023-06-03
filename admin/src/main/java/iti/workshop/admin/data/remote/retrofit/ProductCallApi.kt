6084afbf5cbf0e1329bcf8a37ff99cf506d4c107
Mohamed <dev.mohamed.arfa@gmail.com>
2023-06-03 07:20:57 +0300

    - Add getProduct to ProductCallApi interface

--- admin/src/main/java/iti/workshop/admin/data/remote/retrofit/ProductCallApi.kt
+++ admin/src/main/java/iti/workshop/admin/data/remote/retrofit/ProductCallApi.kt
@@ -1,6 +1,11 @@
 package iti.workshop.admin.data.remote.retrofit
 
-interface ProductCallApi {
+import iti.workshop.admin.data.dto.SuccessProductResponse
+import retrofit2.Response
+import retrofit2.http.GET
 
+interface ProductCallApi {
+    @GET("products.json")
+    fun getProduct():Response<SuccessProductResponse>
 
 }
