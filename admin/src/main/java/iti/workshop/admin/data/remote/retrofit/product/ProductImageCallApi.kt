6ec0ecc354279b936534b6a47d9afd041c89533f
Mohamed <dev.mohamed.arfa@gmail.com>
2023-06-03 12:24:23 +0300

    - write product image methods api

--- admin/src/main/java/iti/workshop/admin/data/remote/retrofit/product/ProductImageCallApi.kt
+++ admin/src/main/java/iti/workshop/admin/data/remote/retrofit/product/ProductImageCallApi.kt
@@ -1,3 +1,22 @@
 package iti.workshop.admin.data.remote.retrofit.product
 
-interface ProductImageCallApi
+import iti.workshop.admin.data.dto.*
+import retrofit2.Response
+import retrofit2.http.*
+
+interface ProductImageCallApi{
+    @GET("products.json")
+    suspend fun getProduct(): Response<SuccessProductResponse>
+
+    @POST("products.json")
+    suspend fun addProduct(@Body data: PostProduct): Response<Product>
+
+    @PUT("products/{id}.json")
+    suspend fun updateProduct(@Path("id") id:Long, @Body data: UpdateProduct): Response<Product>
+
+    @DELETE("products/{id}.json")
+    suspend fun deleteProduct(@Path("id") id:Long): Response<Void>
+
+    @GET("products/count.json")
+    suspend fun getCount(): Response<Count>
+}
