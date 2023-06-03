9c98a6c43f65b3fc94f892bd1d73a1d0373cc58b
AhmedAbdo12345 <Ahmedabdo12377@gmail.com>
2023-06-03 14:13:27 +0300

    create Brand Diff utils class

--- app/src/main/java/iti/mad/marketly/presentation/view/MainActivity.kt
+++ app/src/main/java/iti/mad/marketly/presentation/view/MainActivity.kt
@@ -1,4 +1,4 @@
-package iti.mad.marketly.presentation
+package iti.mad.marketly.presentation.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
---
+++ app/src/main/java/iti/mad/marketly/presentation/view/adapter/BrandsAdapter.kt
@@ -0,0 +1,11 @@
+package iti.mad.marketly.presentation.view.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+
+class BrandsAdapter{
+
+}
