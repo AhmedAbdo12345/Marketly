bb2850cb96a503c9dea366867e092f8aa613751a
Mahmoud Sayed <62342493+MahmoudSayedAwad@users.noreply.github.com>
2023-06-04 05:29:49 +0300

    add auth RemoteDataSource

--- app/src/main/java/iti/mad/marketly/data/source/remote/retrofit/CallApi.kt
+++ app/src/main/java/iti/mad/marketly/data/source/remote/retrofit/CallApi.kt
@@ -1,6 +1,17 @@
 package iti.mad.marketly.data.source.remote.retrofit
 
+import iti.mad.marketly.data.model.CustomerBody
+import iti.mad.marketly.data.model.CustomerResponse
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Query
+
 
 interface CallApi {
+    @POST("customers.json")
+    suspend fun registerUser(@Body customerBody: CustomerBody): CustomerBody
+    @GET("customers.json")
+    suspend fun loginWithEmail(@Query(value = "email:") email:String):CustomerResponse
 
 }
