6345448a72ced86e38c548b1dafdd944a13c4030
AhmedAbdo12345 <Ahmedabdo12377@gmail.com>
2023-06-08 18:23:26 +0300

    Create order Fragment Design XML and Create Adapter class and Diff Utils Class

---
+++ app/src/main/java/iti/mad/marketly/presentation/order/OrderDiffUtils.kt
@@ -0,0 +1,14 @@
+package iti.mad.marketly.presentation.order
+
+import androidx.recyclerview.widget.DiffUtil
+import iti.mad.marketly.data.model.categoryProduct.Product
+
+class OrderDiffUtils  : DiffUtil.ItemCallback<Product>()  {
+    override fun areItemsTheSame(oldItem: Product, newItem: Product): Boolean {
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(oldItem: Product, newItem: Product): Boolean {
+        return oldItem == newItem
+    }
+}
