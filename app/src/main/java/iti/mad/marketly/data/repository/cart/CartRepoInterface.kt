ddc89563fcb007cfa3db166a14176eae3acea610
husonic <sonictwester@gmail.com>
2023-06-15 15:14:11 +0300

    added cart implementation

--- app/src/main/java/iti/mad/marketly/data/repository/cart/CartRepoInterface.kt
+++ app/src/main/java/iti/mad/marketly/data/repository/cart/CartRepoInterface.kt
@@ -1,10 +1,13 @@
 package iti.mad.marketly.data.repository.cart
 
+import iti.mad.marketly.data.model.cart.CartModel
 import iti.mad.marketly.data.model.product.Product
 import iti.mad.marketly.data.model.settings.Address
 import kotlinx.coroutines.flow.Flow
 
 interface CartRepoInterface {
-    fun saveCartProduct(product: Product)
-    suspend fun getAllCartProducts(): Flow<List<Product>>
+    fun saveCartProduct(cartModel: CartModel)
+    suspend fun getAllCartProducts(): Flow<List<CartModel>>
+    fun deleteCartItem(cartID:String)
+
 }
