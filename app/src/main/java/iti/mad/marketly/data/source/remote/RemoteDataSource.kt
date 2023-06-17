3ad26dca5f66722a3b731f8630a736e014a05987
Mahmoud Sayed <62342493+MahmoudSayedAwad@users.noreply.github.com>
2023-06-17 13:19:56 +0300

    fix conflict

--- app/src/main/java/iti/mad/marketly/data/source/remote/RemoteDataSource.kt
+++ app/src/main/java/iti/mad/marketly/data/source/remote/RemoteDataSource.kt
@@ -8,12 +8,9 @@ import com.google.firebase.ktx.Firebase
 import iti.mad.marketly.data.model.cart.CartModel
 import iti.mad.marketly.data.model.customer.CustomerBody
 import iti.mad.marketly.data.model.customer.CustomerResponse
-import iti.mad.marketly.data.model.settings.CurrencyResponse
 import iti.mad.marketly.data.model.favourites.FavouriteResponse
 import iti.mad.marketly.data.model.order.OrderModel
 import iti.mad.marketly.data.model.product.Product
-import iti.mad.marketly.data.source.remote.retrofit.ApiService
-import iti.mad.marketly.utils.Constants
 import iti.mad.marketly.data.model.productDetails.ProductDetails
 import iti.mad.marketly.data.model.settings.Address
 import iti.mad.marketly.data.model.settings.CurrencyResponse
@@ -113,18 +110,109 @@ class RemoteDataSource(
         val db = Firebase.firestore.collection("cart").document(SettingsManager.getDocumentID())
             .collection("CartProduct").get().await()
 
-        val cartResponse : MutableList<CartModel> = mutableListOf()
-        for(items in db.documents){
-            cartResponse.add(CartModel(
-                items.get("id") as Long,
-                items.get("imageURL") as String,
-                items.get("quantity") as Long,
-                items.get("price") as Double,
-                items.get("title") as String
-            ))
+        val cartResponse: MutableList<CartModel> = mutableListOf()
+        for (items in db.documents) {
+            cartResponse.add(
+                CartModel(
+                    items.get("id") as Long,
+                    items.get("imageURL") as String,
+                    items.get("quantity") as Long,
+                    items.get("price") as Double,
+                    items.get("title") as String
+                )
+            )
         }
 
         emit(cartResponse)
     }
+
+    override fun deleteAddress(addressID: String) {
+        val db = Firebase.firestore
+        db.collection("settings").document(SettingsManager.getDocumentID())
+            .collection("Addresses").document(addressID).delete().addOnSuccessListener {
+                Log.i("DeleteAddress", "deleteAddress: DELETED")
+            }.addOnFailureListener {
+                Log.i("DeleteAddress", "deleteAddress: ${it.localizedMessage}")
+            }
     }
+
+    override fun saveAddress(address: Address) {
+        val db = Firebase.firestore
+
+
+        SettingsManager.fillAddress(address)
+
+
+        db.collection("settings").document(SettingsManager.getDocumentID())
+            .collection("Addresses").document(address.AddressID).set(address).addOnSuccessListener {
+                Log.i("FireBassSuccess", "saveAddress: Data Saved")
+            }.addOnFailureListener {
+                Log.i("FireBassFailure", "saveAddress:${it.localizedMessage}")
+            }
+    }
+
+    override fun saveCartProduct(cartModel: CartModel) {
+        val db = Firebase.firestore
+        db.collection("cart").document(SettingsManager.getDocumentID())
+            .collection("CartProduct").document(cartModel.id.toString()).set(cartModel)
+            .addOnSuccessListener {
+                Log.i("FireBassSuccess", "saveProduct: Data Saved")
+            }.addOnFailureListener {
+                Log.i("FireBassFailure", "saveProduct:${it.message}")
+            }
+    }
+
+    override fun deleteCartItem(cartID: String) {
+        val db = Firebase.firestore
+        db.collection("cart").document(SettingsManager.getDocumentID())
+            .collection("CartProduct").document(cartID).delete().addOnSuccessListener {
+                Log.i("DeleteCart", "deleteCart: DELETED")
+            }.addOnFailureListener {
+                Log.i("DeleteAddress", "deleteAddress: ${it.localizedMessage}")
+            }
+    }
+
+    //-------------------------------------------------------------------------------
+    override fun saveProductInOrder(orderModel: OrderModel) {
+        val db = Firebase.firestore
+
+        FirebaseAuth.getInstance().currentUser?.let {
+            var collectionReference =
+                db.collection("orders").document(it.email.toString()).collection("orderList")
+
+            collectionReference.document(orderModel.orderID)
+                .set(orderModel).addOnSuccessListener {
+                    Log.i("zxcvb", "saveProduct: Data Saved")
+                }.addOnFailureListener {
+                    Log.i("zxcvb", "saveProduct:${it.localizedMessage}")
+                }
+
+        }
+    }
+
+    override suspend fun getAllOrders(): Flow<List<OrderModel>> = flow {
+        FirebaseAuth.getInstance().currentUser?.let {
+            val db =
+                Firebase.firestore.collection("orders").document(it.email.toString())
+                    .collection("orderList").get().await()
+
+            val orderResponse: MutableList<OrderModel> = mutableListOf()
+            for (items in db.documents) {
+                val data = items.toObject<OrderModel>()
+                /* if (data != null) {
+                     orderResponse.add(OrderModel(
+                         data.get("orderID") as String,
+                         data.get("itemList") as List<CartModel>,
+                         data.get("itemCount") as Int,
+                         data.get("date") as String))
+                 }*/
+                orderResponse.add(data!!)
+                Log.d("zxcv", "getAllOrders: " + data)
+            }
+
+            emit(orderResponse)
+        }
+
+    }
+}
 
