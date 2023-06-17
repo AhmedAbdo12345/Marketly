3686881a6e4dcf448e3d1b4d5b09b9fae97c9793
AhmedAbdo12345 <Ahmedabdo12377@gmail.com>
2023-06-17 19:12:46 +0300

    implement Unit testing for Category Repository

--- app/src/main/java/iti/mad/marketly/data/repository/category/CategoryRepoImpl.kt
+++ app/src/main/java/iti/mad/marketly/data/repository/category/CategoryRepoImpl.kt
@@ -2,9 +2,7 @@ package iti.mad.marketly.data.repository.category
 
 import iti.mad.marketly.data.model.category.CategoryResponse
 import iti.mad.marketly.data.source.remote.IRemoteDataSource
-import iti.mad.marketly.data.source.remote.retrofit.ApiService
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
 
 class CategoryRepoImpl(val remote: IRemoteDataSource): CategoryRepo {
     override suspend fun getCategory(): Flow<CategoryResponse> = remote.getCategory()
