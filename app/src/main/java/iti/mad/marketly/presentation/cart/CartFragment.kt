32db07e72a0f4769f4020d250793f5bdcc2fde84
Hussien Ahmed <45578079+HusseinAhmedAbdelhammed@users.noreply.github.com>
2023-06-21 13:56:15 +0300

    Fixed Order and payment methods

--- app/src/main/java/iti/mad/marketly/presentation/cart/CartFragment.kt
+++ app/src/main/java/iti/mad/marketly/presentation/cart/CartFragment.kt
@@ -108,10 +108,10 @@ class CartFragment : Fragment(), CartFragmentInterface {
         binding.checkoutBtn.setOnClickListener(View.OnClickListener {
             val itemCount = adapters.itemCount
             val currentItems = adapters.currentList
+            DraftOrderManager.setOrder(currentItems)
             var totalPrice = 0.0
             val lineItemList = mutableListOf<LineItems>()
             for (items in currentItems) {
-
                 totalPrice += items.price
                 lineItemList.add(LineItems(items.price.toString(),items.id.toInt(),items.quantity.toInt(),"Custom "+items.title))
             }
@@ -130,9 +130,7 @@ class CartFragment : Fragment(), CartFragmentInterface {
             }
             val customer = iti.mad.marketly.data.model.draftorder.Customer(customerID.toLong(),false)
             DraftOrderManager.setCustomer(customer)
-            val orderID = System.currentTimeMillis().toString()
-            val order = OrderModel(orderID, currentItems, itemCount, DateFormatter.getCurrentDate(),totalPrice)
-            cartViewModel.saveProuctsInOrder(order)
+
             var action= CartFragmentDirections.actionCartFragment2ToDraftAddressFragment()
             findNavController().navigate(action)
 
--- app/src/main/java/iti/mad/marketly/presentation/draftorder/ChecoutFragmentCredit.kt
+++ app/src/main/java/iti/mad/marketly/presentation/draftorder/ChecoutFragmentCredit.kt
@@ -1,17 +1,48 @@
 package iti.mad.marketly.presentation.draftorder
 
+import android.content.ContentValues
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.findNavController
 import iti.mad.marketly.R
-
+import iti.mad.marketly.data.draftOrderInvoice.DraftOrderInvoice
+import iti.mad.marketly.data.draftOrderInvoice.DraftOrderInvoiceX
+import iti.mad.marketly.data.model.draftorder.DraftOrderRequest
+import iti.mad.marketly.data.model.order.OrderModel
+import iti.mad.marketly.data.source.local.sharedpreference.SharedPreferenceManager
+import iti.mad.marketly.databinding.FragmentCheckoutBinding
+import iti.mad.marketly.databinding.FragmentChecoutCreditBinding
+import iti.mad.marketly.presentation.cart.CartViewModel
+import iti.mad.marketly.utils.AdsManager
+import iti.mad.marketly.utils.AlertManager
+import iti.mad.marketly.utils.Constants
+import iti.mad.marketly.utils.CurrencyConverter
+import iti.mad.marketly.utils.DateFormatter
+import iti.mad.marketly.utils.DraftOrderManager
+import iti.mad.marketly.utils.ResponseState
+import iti.mad.marketly.utils.SettingsManager
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 
 class ChecoutFragmentCredit : Fragment() {
 
-
+    lateinit var binding: FragmentChecoutCreditBinding
+    private val draftOrderViewModel by viewModels<DraftOrderViewModel> {
+        DraftOrderViewModel.Factory
+    }
+    private val cartViewModel by viewModels<CartViewModel>{
+        CartViewModel.Factory
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -21,8 +52,129 @@ class ChecoutFragmentCredit : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_checout_credit, container, false)
+        binding = FragmentChecoutCreditBinding.inflate(layoutInflater,container,false)
+        binding.lifecycleOwner=viewLifecycleOwner
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.appBar.backArrow.setOnClickListener(View.OnClickListener {
+            findNavController().navigateUp()
+        })
+        binding.creditCardPage.setOnClickListener(View.OnClickListener {
+            val action = ChecoutFragmentCreditDirections.actionChecoutFragmentCreditToCreditFragment()
+            findNavController().navigate(action)
+        })
+        val draftOrderLineItemList = DraftOrderManager.getLineList()
+        val draftOrderAddress = DraftOrderManager.getShippingAddress()
+        val draftOrderCustomer = DraftOrderManager.getCustomer()
+        val draftAppliedDiscount = DraftOrderManager.getDiscount()
+        var currency:String = SharedPreferenceManager.getSavedCurrency(requireContext()).toString()
+        var totalAmount = 0.0
+        val invoice = DraftOrderInvoiceX(
+            Constants.EMAIL_BODY,
+            Constants.EMAIL_SUBJECT,
+            SharedPreferenceManager.getUserMAil(requireContext()).toString())
+        for (item in draftOrderLineItemList){
+            totalAmount =totalAmount+item.price.toDouble()
+            Toast.makeText(requireContext(),"${totalAmount}", Toast.LENGTH_LONG).show()
+        }
+        if(currency == null||currency == ""){
+            currency = "USD"
+        }
+        val draftOrder= DraftOrderManager.buildDraftOrder(draftOrderLineItemList,draftOrderCustomer,draftOrderAddress,currency,draftAppliedDiscount)
+        draftOrderViewModel.createDraftOrder(DraftOrderRequest(draftOrder))
+        viewLifecycleOwner.lifecycleScope.launch (Dispatchers.Main){
+            repeatOnLifecycle(Lifecycle.State.STARTED){
+                draftOrderViewModel._draftResponse.collect{
+                    when(it){
+                        is ResponseState.OnLoading->{
 
+                        }
+                        is ResponseState.OnSuccess->{
+                            DraftOrderManager.setDraftOrderID(it.response.draft_order.id)
+                            Toast.makeText(requireContext(),"Order Created Successfully", Toast.LENGTH_LONG).show()
+                        }
+                        is ResponseState.OnError->{
+                            Log.i(ContentValues.TAG, "onViewCreated:${it.message} ")
+                            Toast.makeText(requireContext(),it.message, Toast.LENGTH_LONG).show()
+                        }
+                        else ->{}
+                    }
+                }
+            }
+        }
+        binding.countryName.text = draftOrderAddress.country
+        binding.city.text = draftOrderAddress.city
+        binding.discountText.text = draftAppliedDiscount.title
+        binding.discountValueFromApi.text = draftAppliedDiscount.value
+        val percentage = (totalAmount)*(AdsManager.value/100)
+        totalAmount = totalAmount - percentage
+
+
+        if(SettingsManager.getCurrncy()=="EGP"){
+            binding.totalAmountTextView.text= CurrencyConverter.switchToEGP(totalAmount.toString(), binding.totalAmountTextView.id)+" LE"
+        }else{
+            binding.totalAmountTextView.text= CurrencyConverter.switchToUSD(totalAmount.toString(), binding.totalAmountTextView.id)+" $"
+        }
+
+
+
+        val method = {
+
+            if(DraftOrderManager.checkCreditCard()){
+                draftOrderViewModel.sendInvoice(
+                    DraftOrderInvoice(invoice),
+                    DraftOrderManager.getDraftOrderID().toString())
+                draftOrderViewModel.completeOrder(DraftOrderManager.getDraftOrderID().toString())
+                cartViewModel.getAllCart()
+                viewLifecycleOwner.lifecycleScope.launch(Dispatchers.Main) {
+                    repeatOnLifecycle(Lifecycle.State.STARTED) {
+                        cartViewModel._cartResponse.collect {
+
+                            when (it) {
+                                is ResponseState.OnLoading -> {
+                                }
+                                is ResponseState.OnSuccess -> {
+                                    var cartItems = it.response.toMutableList()
+
+                                    for (carts in it.response){
+                                        cartViewModel.deleteCartItem(carts.id.toString())
+                                    }
+
+                                }
+                                is ResponseState.OnError -> {
+                                    Log.i(
+                                        ContentValues.TAG,
+                                        "onViewCreated:${it.message} this is an errrror"
+                                    )
+                                }
+                                else -> {}
+                            }
+
+                        }
+                    }
+                }
+                AlertManager.customDialog("Completed",requireContext(),"CONGRATULATIONS",{
+
+                    val orderID = System.currentTimeMillis().toString()
+
+                    val order = OrderModel(orderID,DraftOrderManager.getOrder(),DraftOrderManager.getOrder().size, DateFormatter.getCurrentDate(),totalAmount)
+                    cartViewModel.saveProuctsInOrder(order)
+                    DraftOrderManager.clearCredit()
+                    val action = ChecoutFragmentCreditDirections.actionChecoutFragmentCreditToHomeFragment()
+                    findNavController().navigate(action)
+                }).show()
+            }else{
+                AlertManager.nonFunctionalDialog("ERROR",requireContext(),"Please Enter a valid Credit Card")
+            }
+
+
+        }
+        binding.placeOrder.setOnClickListener(View.OnClickListener {
+            AlertManager.functionalDialog("Order Confermation",requireContext(),"Do you want to checkout?",method).show()
+
+        })
+    }
 }
