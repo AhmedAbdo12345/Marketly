7806711d3df59f80965474db5756d13499d53a0e
AhmedAbdo12345 <Ahmedabdo12377@gmail.com>
2023-06-21 11:38:39 +0300

    fix some bugs

--- app/src/main/java/iti/mad/marketly/presentation/home/HomeFragment.kt
+++ app/src/main/java/iti/mad/marketly/presentation/home/HomeFragment.kt
@@ -32,11 +32,11 @@ import iti.mad.marketly.data.model.cart.CartModel
 import iti.mad.marketly.data.source.local.sharedpreference.SharedPreferenceManager
 import iti.mad.marketly.databinding.FragmentHomeBinding
 import iti.mad.marketly.presentation.cart.CartViewModel
+import iti.mad.marketly.presentation.favourite.FavouriteFragmentDirections
 import iti.mad.marketly.presentation.home.ads.AdsViewModel
 import iti.mad.marketly.presentation.home.brands.BrandsAdapter
 import iti.mad.marketly.presentation.home.brands.BrandsViewModel
 import iti.mad.marketly.presentation.settings.SettingsViewModel
-
 import iti.mad.marketly.utils.AdsManager
 import iti.mad.marketly.utils.AlertManager
 import iti.mad.marketly.utils.NetworkConnectivityChecker
@@ -124,8 +124,15 @@ class HomeFragment : Fragment(), BrandsAdapter.ListItemClickListener {
                 return when (menuItem.itemId) {
                     R.id.cartIcon -> {
                         // Handle the item click here
-                        /*Log.d("cart","navigate")
-                        findNavController().navigate(R.id.cartFragment)*/
+                        /*Log.d("cart","navigate")*/
+                       // binding.notLoginConstraint.visibility = View.GONE
+                        if (SharedPreferenceManager.isUserLogin(requireContext())) {
+                      findNavController().navigate(R.id.cartFragment2)
+                  }else{
+                     // menuItem.isEnabled = false
+                   //   binding.notLoginConstraint.visibility = View.VISIBLE
+                    //  handleRegister()
+                  }
                         true
                     }
 
@@ -331,6 +338,18 @@ class HomeFragment : Fragment(), BrandsAdapter.ListItemClickListener {
             )!!
         )
     }
+  /*  fun handleRegister() {
+        binding.loginBtn.setOnClickListener {
+            val action = FavouriteFragmentDirections.actionFavouriteFragmentToLoginFragment()
+            findNavController().navigate(action)
+        }
+        binding.registerBtn.setOnClickListener {
+            val action = FavouriteFragmentDirections.actionFavouriteFragmentToRegisterFragment()
+            findNavController().navigate(action)
+
+
+        }
+    }*/
 }
 
 
