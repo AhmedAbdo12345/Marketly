8bf7f83c471666ac1b8af90c9489a655380bb201
AhmedAbdo12345 <Ahmedabdo12377@gmail.com>
2023-06-22 11:51:33 +0300

    fix bugs

---
+++ app/src/main/java/iti/mad/marketly/data/FirebaseService.kt
@@ -0,0 +1,104 @@
+package iti.mad.marketly.data
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.NotificationManager.IMPORTANCE_HIGH
+import android.app.PendingIntent
+import android.app.PendingIntent.FLAG_ONE_SHOT
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+import android.graphics.Color
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import iti.mad.marketly.R
+import iti.mad.marketly.presentation.MainActivity
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlin.random.Random
+
+private const val CHANNEL_ID = "vf_channel"
+
+class FirebaseService : FirebaseMessagingService() {
+
+    companion object {
+        var sharedPref: SharedPreferences? = null
+
+        var token: String?
+            get() {
+                return sharedPref?.getString("token", "")
+            }
+            set(value) {
+                sharedPref?.edit()?.putString("token", value)?.apply()
+            }
+    }
+
+    override fun onNewToken(newToken: String) {
+        super.onNewToken(newToken)
+        token = newToken
+    }
+
+    @OptIn(DelicateCoroutinesApi::class)
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
+//        val user: User = User().getUser(this)
+//        if (user.is_admin != true && user.is_superuser != true){
+//            return
+//        }
+        val intent = Intent(this, MainActivity::class.java)
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val notificationID = Random.nextInt()
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            createNotificationChannel(notificationManager)
+        }
+
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+
+
+        val pendingIntent = PendingIntent.getActivity(
+            this,
+            0,
+            intent,
+            FLAG_ONE_SHOT or PendingIntent.FLAG_IMMUTABLE
+        )
+        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentTitle(message.data["title"])
+            .setContentText(message.data["message"])
+            .setSmallIcon(R.drawable.category)
+            .setAutoCancel(true)
+            .setContentIntent(pendingIntent)
+            .build()
+
+        notificationManager.notify(notificationID, notification)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotificationChannel(notificationManager: NotificationManager) {
+        val channelName = "Vodafone Cash"
+        val channel = NotificationChannel(CHANNEL_ID, channelName, IMPORTANCE_HIGH).apply {
+            description = "My channel description"
+            enableLights(true)
+            lightColor = Color.GREEN
+        }
+        notificationManager.createNotificationChannel(channel)
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
